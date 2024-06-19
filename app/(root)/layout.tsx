7568aeb63d0c23d4bb22acd313dc874a0a6f7d58
g0dlessjs <juangalarce.dev@gmail.com>
2024-06-19 10:03:12 -0400

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      <div className="wrapper">{children}</div>
+    </main>
+  );
+};
+export default Layout;
---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+import { Variable } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex",
+});
+
+export const metadata: Metadata = {
+  title: "Art4IA",
+  description: "AI-powered image generator",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
